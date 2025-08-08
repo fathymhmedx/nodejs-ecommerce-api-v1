68beaf9fa21a2b057236312eb7d389d292203dd8
Fathy Mohamed <fathymhmed11@gmail.com>
2025-08-08 21:23:41 +0300

    Refactor (Controller, REDME): Advanced Filtering & Add REDME file

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -66,14 +66,37 @@ exports.createProduct = asyncHandler(async (req, res) => {
  */
 exports.getProducts = asyncHandler(async (req, res) => {
     // 1) Filtering 
+    // BUILD QUERY
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach( field => delete queryObj[field]);
-    
+    excludedFields.forEach( el => delete queryObj[el]);
+
+    // 2) Advanced Filtering (price[gte] => price: { $gte: value }) 
+
+    let mongoQuery = {};
+
+    for (const key in queryObj) {
+        const value = isNaN(queryObj[key]) ? queryObj[key] : Number(queryObj[key]);
+
+        const match = key.match(/^(.+)\[(gte|gt|lte|lt)\]$/);
+        if (match) {
+            const [, field, operator] = match;
+            if (!mongoQuery[field]) mongoQuery[field] = {};
+            mongoQuery[field][`$${operator}`] = value;
+        } else {
+            if (typeof value === 'string') {
+                mongoQuery[key] = { $regex: value, $options: 'i' }; 
+            } else {
+                mongoQuery[key] = value;
+            }
+        }
+
+    }
+    // 2) Pagination
     const { page, limit, skip } = getPagination(req.query);
     const [total, products] = await Promise.all([
-        Product.countDocuments(),
-        Product.find().skip(skip).limit(limit).lean()
+        Product.countDocuments(mongoQuery),
+        Product.find(mongoQuery).skip(skip).limit(limit).lean()
         // .populate({ path: 'category', select: 'name -_id' })
     ])
 
