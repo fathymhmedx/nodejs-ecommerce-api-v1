b787d848ab2e35f6ccd41aed1a5bab3b6666d180
Fathy Mohamed <fathymhmed11@gmail.com>
2025-09-01 22:43:42 +0300

    Add security middlewares and update API route
    - Added xss-clean, hpp, and express-mongo-sanitize for security
    - Applied them in app.js
    - Changed route import from `routes` to `v1Routes`

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
+
 // const rateLimit = require('express-rate-limit');
 const dotenv = require('dotenv');
 
@@ -11,10 +16,19 @@ dotenv.config({ path: "./config.env" });
 // 2. App initialization
 const app = express();
 
+// Disable X-Powered-By header
+app.disable('x-powered-by');
+
 // 3. Global middlewares
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, './src/uploads')));
+
+app.use(xss());
+app.use(mongoSanitize());
 app.use(helmet());
+app.use(hpp());
+
 app.use(cors({
     origin: "*",
     // Allow all origins (change to specific domain in production, like: origin: process.env.CLIENT_URL)
@@ -26,8 +40,8 @@ const { initLogger } = require('./src/shared/config/logger.js')
 initLogger(app);
 
 // 5. Mount routes
-const routes = require('./src/routes');
-app.use('/api/v1/', routes);
+const v1Routes = require('./src/routes');
+app.use('/api/v1/', v1Routes);
 
 // 6. Global Error Handling
 const { notFoundHandler, globalErrorHandler } = require('./src/shared/middlewares/errorMiddleware.js');
