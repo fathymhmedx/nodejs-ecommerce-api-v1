5fcf964297eb0b0b669fd37ea40e6210ed8d6701
Fathy Mohamed <fathymhmed11@gmail.com>
2025-12-03 03:14:03 +0200

    chore(routes): Added  coupon and cart routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,9 @@ const subCategoryRoutes = require('../modules/subCategory/subCategory.routes');
 const userRoutes = require('../modules/user/user.routes');
 const reviewRoutes = require('../modules/review/review.routes');
 const wishlistRoutes = require('../modules/user/wishlist/wishlist.routes');
-const addressRoutes = require('../modules/user/addresses/addresses.routes')
+const addressRoutes = require('../modules/user/addresses/addresses.routes');
+const couponRoutes = require('../modules/coupon/coupon.routes');
+const cartRoutes = require('../modules/cart/cart.routes');
 
 router.use('/auth', authRoutes);
 router.use('/categories', categoryRoutes);
@@ -20,5 +22,7 @@ router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
 router.use('/wishlist', wishlistRoutes);
 router.use('/addresses', addressRoutes);
+router.use('/coupons', couponRoutes);
+router.use('/cart', cartRoutes);
 
 module.exports = router;
